e1aeceaee73827527dee255e0b64c0d96bff747a
MaysonLima <Maysonlima021@gmail.com>
2026-04-20 17:07:59 -0300

    feature: fazer a geração de imagens

--- Sermo/src/App.jsx
+++ Sermo/src/App.jsx
@@ -1,9 +1,35 @@
 import { Sidebar } from './components/sidebar/index'
 import { SermoForm } from './components/SermoForm/index'
 import { Sermo } from './components/Sermo/index'
+import { v4 } from "uuid"
+import { getAvatar, getRandomImage } from './utils/generateImages'
+import { useState } from 'react'
 
 
 function App() {
+  const[sermos, setSermos] = useState([])
+
+  const addNewSermo = (content, includeImage = false) => {
+    const newSermo = {
+      id: v4(),
+      name: "User",
+      username: `users${Math.floor(Math.random() * 1000)}`,
+      avatar: getAvatar(`user${Math.floor (Math.random() * 1000)}@email.com`),
+      content,
+      time: new Date().toLocaleString([],{
+        hour: '2-digit',
+        minute: '2-digit'
+      }),
+      image: includeImage ?  getRandomImage() : null,
+      likes: 0,
+      resermos: 0,
+      comments: 0
+
+  }
+
+  setSermos(prevSermos => [newSermo, ...prevSermos])
+
+  }
 
   return (
 
@@ -13,7 +39,7 @@ function App() {
         <header className='sticky top-0 z-10 bg-sermo-background bg-opacity-80 backdrop-blur-sm border-b border-gray-700'>
           <h2 className='px-4 py-3 text-xl font-bold text-white'>For You</h2>
         </header>
-        <SermoForm/>
+        <SermoForm onSermo={ (content) => addNewSermo(content, Math.random() > 0.8)}/>
         <div>
           <Sermo />
         </div>
